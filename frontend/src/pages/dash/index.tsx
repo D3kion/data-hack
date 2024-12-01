9772de79f0a5f33433058f3d637f953ac67a155d
D3kion <dekion.dev@gmail.com>
2024-12-01 11:54:48 +0300

    feat: mock dash page

--- frontend/src/pages/dash/index.tsx
+++ frontend/src/pages/dash/index.tsx
@@ -9,22 +9,32 @@ import {
 } from "@/shared/ui-kit";
 
 const chartData = [
-  { month: "January", desktop: 186, mobile: 80 },
-  { month: "February", desktop: 305, mobile: 200 },
-  { month: "March", desktop: 237, mobile: 120 },
-  { month: "April", desktop: 73, mobile: 190 },
-  { month: "May", desktop: 209, mobile: 130 },
-  { month: "June", desktop: 214, mobile: 140 },
+  { product: "Кредит для бизнеса 1", transactionsSum: 6643586088 },
+  { product: "Кредит для бизнеса 2", transactionsSum: 1630923494 },
+  { product: "Кредит для физлиц 1", transactionsSum: 778457300 },
+  { product: "Кредит для физлиц 2", transactionsSum: 629776401 },
+  { product: "Кредит для физлиц 3", transactionsSum: 401081057 },
+];
+
+const chart2Data = [
+  { product: "Кредит для бизнеса 1", count: 624 },
+  { product: "Кредит для бизнеса 2", count: 675 },
+  { product: "Кредит для физлиц 1", count: 408 },
+  { product: "Кредит для физлиц 2", count: 370 },
+  { product: "Кредит для физлиц 3", count: 422 },
 ];
 
 const chartConfig = {
-  desktop: {
-    label: "Desktop",
+  transactionsSum: {
+    label: "Сумма транзакций",
     color: "#2563eb",
   },
-  mobile: {
-    label: "Mobile",
-    color: "#60a5fa",
+} satisfies ChartConfig;
+
+const chart2Config = {
+  count: {
+    label: "Количество клиентов",
+    color: "#2563eb",
   },
 } satisfies ChartConfig;
 
@@ -37,11 +47,9 @@ export default function DashboardPage() {
         </h2>
         {/* EXPORT */}
       </div>
-      <div className="grid lg:grid-cols-2 gap-4">
+      <div className="grid gap-4">
         <Chart />
-        <Chart />
-        <Chart />
-        <Chart />
+        <Chart2 />
       </div>
     </div>
   );
@@ -51,22 +59,50 @@ function Chart() {
   return (
     <div className="border rounded">
       <h3 className="text-2xl font-semibold tracking-tight text-center py-2 px-4">
-        Metric #1
+        Сумму транзакций по типу кредита
       </h3>
       <ChartContainer config={chartConfig} className="min-h-[200px] w-full ">
         <BarChart data={chartData}>
           <CartesianGrid vertical={false} />
           <XAxis
-            dataKey="month"
+            dataKey="product"
             tickLine={false}
             tickMargin={10}
             axisLine={false}
-            tickFormatter={(value) => value.slice(0, 3)}
           />
           <ChartTooltip content={<ChartTooltipContent />} />
           <ChartLegend content={<ChartLegendContent />} />
-          <Bar dataKey="desktop" fill="var(--color-desktop)" radius={4} />
-          <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} />
+          <Bar
+            dataKey="transactionsSum"
+            fill="var(--color-transactionsSum)"
+            radius={4}
+          />
+          {/* <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} /> */}
+        </BarChart>
+      </ChartContainer>
+    </div>
+  );
+}
+
+function Chart2() {
+  return (
+    <div className="border rounded">
+      <h3 className="text-2xl font-semibold tracking-tight text-center py-2 px-4">
+        Распределение типов кредитных продуктов по клиентам (по количеству)
+      </h3>
+      <ChartContainer config={chart2Config} className="min-h-[200px] w-full ">
+        <BarChart data={chart2Data}>
+          <CartesianGrid vertical={false} />
+          <XAxis
+            dataKey="product"
+            tickLine={false}
+            tickMargin={10}
+            axisLine={false}
+          />
+          <ChartTooltip content={<ChartTooltipContent />} />
+          <ChartLegend content={<ChartLegendContent />} />
+          <Bar dataKey="count" fill="var(--color-count)" radius={4} />
+          {/* <Bar dataKey="mobile" fill="var(--color-mobile)" radius={4} /> */}
         </BarChart>
       </ChartContainer>
     </div>
